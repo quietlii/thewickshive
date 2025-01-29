4668b6511cc3b5f503149f3ed8058d2a4382cced
quietlii <86536562+quietlii@users.noreply.github.com>
2025-01-28 19:09:07 -0500

    Edit

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1jpGFxbmIhIiz7gaYFAOiLR_8WkUK8bgynVxKJu7ZY-E";
 
 
 /* ------------------------------------------------------------------- */
